4c4fde99a4f4700cfa4977bf22ef93402cfe24df
Will 保哥 <doggy.huang@gmail.com>
2019-07-06 17:10:42 +0800

    Update index.ts
    Fix wrong numbering

--- 2_schematics_options/hello/src/hello/index.ts
+++ 2_schematics_options/hello/src/hello/index.ts
@@ -22,34 +22,34 @@ export function hello(_options: any): Rule {
 
     console.log('Running schematics with following options', _options);
 
-    // TODO 8. retrieve name property form _options ( store it in a name variable )
+    // TODO 9. retrieve name property form _options ( store it in a name variable )
 
-    // TODO 9. use name variable to parametrize hello.js file content ( replace NG-MY with variable using string template syntax ${} )
+    // TODO 10. use name variable to parametrize hello.js file content ( replace NG-MY with variable using string template syntax ${} )
 
     tree.create('hello.js', `console.log('Hello NG-MY!');`);
 
-    // TODO 10. build and run schematics with --name param to create file (mind --dry-run=false because of the dev mode)
+    // TODO 11. build and run schematics with --name param to create file (mind --dry-run=false because of the dev mode)
 
-    // TODO 11. run created file using node
+    // TODO 12. run created file using node
 
-    // TODO 12. explore running schematics with wrong params like --name1 or without any params at all
+    // TODO 13. explore running schematics with wrong params like --name1 or without any params at all
 
     return tree;
 
-    // TODO 13. add new "greeting" option in both schema.d.ts and schema.json, it will be of a string type
+    // TODO 14. add new "greeting" option in both schema.d.ts and schema.json, it will be of a string type
     // in schema.json we will also list possible values using enum property with an array of possible values like Hello, Ola, or Ahoj
 
-    // TODO 14. add default value (eg Hello) and x-prompt (with the question about greeting we want to use) for the new property in schema.json file
+    // TODO 15. add default value (eg Hello) and x-prompt (with the question about greeting we want to use) for the new property in schema.json file
 
-    // TODO 15. get the new "greeting" property from _options and replace hardcoded "Hello" in the generated file content
+    // TODO 16. get the new "greeting" property from _options and replace hardcoded "Hello" in the generated file content
 
-    // TODO 16. build and run schematics with --name param, but WITHOUT --greeting param (mind --dry-run=false because of the dev mode)
+    // TODO 17. build and run schematics with --name param, but WITHOUT --greeting param (mind --dry-run=false because of the dev mode)
     // you should see that the schmatics will ask you about the missing "greeting" parameter and give you a choice to select value
 
-    // TODO 17. take a look in ./helpers/schema.json_advanced and copy its content to replace properties in schema.json
+    // TODO 18. take a look in ./helpers/schema.json_advanced and copy its content to replace properties in schema.json
     // this adjusts name property definition with "$default" which enables us to pass in name without --name property
     // the name will be taken as a positional property from command line ( "$source": "argv" ) as the first argument ( "index": 0 )
 
-    // TODO 18. try to remove "x-prompt" from greetings and run schematics again to see it will use default value when no --greeting option was provided
+    // TODO 19. try to remove "x-prompt" from greetings and run schematics again to see it will use default value when no --greeting option was provided
   };
 }
